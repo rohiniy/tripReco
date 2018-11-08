2afd38475c9bb37efa90d52fefa477effc33dfdb
Rohini Yadav <ryadav@marketo.com>
2018-11-08 14:03:26 -0800

    Committing changes for recommending similar cities to the user

--- content_based_filetering.py
+++ content_based_filetering.py
@@ -1,5 +1,8 @@
 import pandas
 import numpy
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
 #ignoring warnings
 numpy.seterr(divide='ignore', invalid='ignore')
@@ -13,3 +16,36 @@ placesDataset = pandas.read_csv(placesUrl, names=placesColumns)
 userRatingsUrl = "./user_place_rating.csv"
 userRatingsColumns = ['User_id', 'City_id', 'Rating']
 userRatingsDataset = pandas.read_csv(userRatingsUrl, names=userRatingsColumns)
+
+#get matrix of places with CountVectorizer
+count = CountVectorizer(stop_words='english')
+count_matrix = count.fit_transform(placesDataset)
+#print(count_matrix)
+
+
+# get matrix of places with tfId
+tf = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='english')
+tfidf_matrix = tf.fit_transform(placesDataset['Type'])
+#print(tfidf_matrix)
+
+# get similarity between places based on the type
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+#print(cosine_sim)
+
+# build a 1-dimensional array with City names
+citiesArray = placesDataset['City']
+# create index for each city
+indices = pandas.Series(placesDataset.index, index=placesDataset['City'])
+
+# # Function that get City recommendations based on the cosine similarity score of City's type
+def get_city_recommendations(city):
+  idx = indices[city]
+  sim_scores = list(enumerate(cosine_sim[idx]))
+  sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+  sim_scores = sim_scores[1:21]
+  city_indices = [i[0] for i in sim_scores]
+  return citiesArray.iloc[city_indices]
+
+recommendationArray = get_city_recommendations('Detroit')
+print('Recommendations::', recommendationArray)
+
