1b1bf091e72125c0eb54b91d554e7eddf63a63b6
Anushree Sridhar <asridhar@marketo.com>
2018-11-08 12:40:40 -0800

    Initial commit for content based filtering

---
+++ content_based_filetering.py
@@ -0,0 +1,15 @@
+import pandas
+import numpy
+
+#ignoring warnings
+numpy.seterr(divide='ignore', invalid='ignore')
+
+# get places data set
+placesUrl = "./places.csv"
+placesColumns = ['City_id', 'City', 'Type']
+placesDataset = pandas.read_csv(placesUrl, names=placesColumns)
+
+# get user rating for place
+userRatingsUrl = "./user_place_rating.csv"
+userRatingsColumns = ['User_id', 'City_id', 'Rating']
+userRatingsDataset = pandas.read_csv(userRatingsUrl, names=userRatingsColumns)
