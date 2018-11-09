0d050fcd2cdaa3a32d2971775a6dc90cbe579ad1
Anushree Sridhar <asridhar@marketo.com>
2018-11-08 16:25:53 -0800

    Slightly broken merged ratings + similarity

--- content_based_filetering.py
+++ content_based_filetering.py
@@ -4,6 +4,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
+varToPass = 'City'
 #ignoring warnings
 numpy.seterr(divide='ignore', invalid='ignore')
 
@@ -20,13 +21,13 @@ userRatingsDataset = pandas.read_csv(userRatingsUrl, names=userRatingsColumns)
 userRatingsDataset = pandas.merge(userRatingsDataset, placesDataset, on='City_id')
 
 # mean of the ratings per city
-ratings = pandas.DataFrame(userRatingsDataset.groupby('City')['Rating'].mean())
+ratings = pandas.DataFrame(userRatingsDataset.groupby(varToPass)['Rating'].mean())
 #print(ratings)
 
 # count of ratings per city
-ratings['number_of_ratings'] = userRatingsDataset.groupby('City')['Rating'].count()
+ratings['number_of_ratings'] = userRatingsDataset.groupby(varToPass)['Rating'].count()
 # creating matrix
-place_matrix = userRatingsDataset.pivot_table(index='User_id', columns='City', values='Rating')
+place_matrix = userRatingsDataset.pivot_table(index='User_id', columns=varToPass, values='Rating')
 #print(place_matrix)
 
 #get matrix of places with CountVectorizer
@@ -41,7 +42,7 @@ count_matrix = count.fit_transform(placesDataset['Type'])
 
 # get similarity between places based on the type
 cosine_sim = linear_kernel(count_matrix, count_matrix)
-print(cosine_sim)
+#print(cosine_sim)
 
 # build a 1-dimensional array with City names
 cities = placesDataset['City']
@@ -51,27 +52,30 @@ indices = pandas.Series(placesDataset.index, index=placesDataset['City'])
 # # Function that get City recommendations based on the cosine similarity score of City's type
 def get_city_recommendations(city):
   idx = indices[city]
-  sim_scores = list(enumerate(cosine_sim[idx]))
-  sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-  sim_scores = sim_scores[1:33]
-  #print(sim_scores)
-  city_indices = [i[0] for i in sim_scores]
+  simScores = list(enumerate(cosine_sim[idx]))
+  simScores = sorted(simScores, key=lambda x: x[1], reverse=True)
+  simScores = simScores[1:33]
+  #print(simScores)
+  cityIndices = [i[0] for i in simScores]
+  print(cityIndices)
 
   # simple item based recommender system for New York
-  city_index = placesDataset.loc[placesDataset['City'] == city, 'City_id']
-  #print(city_index)
   city_user_rating = place_matrix[city]
   similar_to_city = place_matrix.corrwith(city_user_rating)
-  corr_city = pandas.DataFrame(similar_to_city, columns=['Correlation'])
-  #corr_city = corr_city.join(ratings['number_of_ratings'])
-  #print(corr_city.sort_values(by='Correlation', ascending=False))
+  corrCity = pandas.DataFrame(similar_to_city, columns=['Correlation'])
+  corrCity = pandas.merge(corrCity, placesDataset, on='City')
+  #corrCity = corrCity.join(ratings['number_of_ratings'])
+  #print(corrCity.sort_values(by='Correlation', ascending=False))
 
-  simScoresDataFrame = pandas.DataFrame(sim_scores)
-  #typeSimScoresRatingCorrDataset = pandas.merge(corr_city, simScoresDataFrame, on='City_id')
-  #print(typeSimScoresRatingCorrDataset)
-  return cities.iloc[city_indices]
+  simScoresDataFrame = pandas.DataFrame(simScores)
+  print(simScoresDataFrame)
+  typeSimScoresRatingCorrDataset = pandas.merge(corrCity, simScoresDataFrame, left_on='City_id', right_on=0)
+  #print(typeSimScoresRatingCorrDataset.sort_values(by=[1, 'Correlation'], ascending=[False, False]))
+  return cities.iloc[cityIndices]
 
-recommendationArray = get_city_recommendations('Las Vegas')
-#print('Recommendations::', recommendationArray)
+
+
+recommendationArray = get_city_recommendations('New York')
+print('Recommendations::', recommendationArray)
 
 
